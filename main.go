a741816fc0a30fecfe689177a5bc3a5b32239a67
George Pugachev <g.v.pugachev@gmail.com>
2019-01-19 15:40:22 +0200

    second commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type iPAddr [4]byte
+
+func (v iPAddr) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d", v[0], v[1], v[2], v[3])
+}
+
+func main() {
+	hosts := map[string]iPAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
